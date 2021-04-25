1b97f69a154feff8d732f8f980f929e79a84f0a9
liuhanru <isuekey@163.com>
2021-04-25 17:10:22 +0800

    开发组件

---
+++ build/rollup.config.js
@@ -0,0 +1,23 @@
+
+import commonjs from "@rollup/plugin-commonjs";
+import vuePlugin from 'rollup-plugin-vue';
+import buble from '@rollup/plugin-buble';
+import svg from 'rollup-plugin-svg';
+
+
+export default {
+  input: 'src/wrapper.js',
+  output: {
+    name: 'DayiWidget',
+    exports: 'named',
+  },
+  plugins:[
+    svg(),
+    commonjs(),
+    vuePlugin({
+      css:true,
+      compileTemplate:true,
+    }),
+    buble(),
+  ],
+};
---
+++ src/wrapper.js
@@ -0,0 +1,27 @@
+
+import CargoWithdraw from './withdraw/amap-route.vue';
+
+export function install(Vue) {
+  if(install.installed) return;
+  install.installed = true;
+  Vue.component('CargoWithdraw', CargoWithdraw);
+}
+
+const plugin = {
+  install,
+};
+
+let GlobalVue = null;
+
+if (typeof window!= 'undefined') {
+  GlobalVue = window.Vue;
+} else if (typeof global != 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+export {
+  CargoWithdraw
+};
+
